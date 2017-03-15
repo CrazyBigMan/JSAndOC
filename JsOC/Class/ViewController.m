3acea46414790818d819ada2fc3d8501558a8376
yayungit <yayun_he@163.com>
2017-03-15 13:19:01 +0800

    updtae JSOC

--- JsOC/Class/ViewController.m
+++ JsOC/Class/ViewController.m
@@ -14,7 +14,7 @@
 
 - (void)hide123;
 - (void)addSomeView:(UIView *)view;
-
+- (int)getStatusBarHeight;
 @end
 
 @interface ViewController ()<UIWebViewDelegate,JSCallOCProtol>
@@ -30,9 +30,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSString *pathStr = [[NSBundle mainBundle] pathForResource:@"jsoc" ofType:@"html"];
+//    NSString *pathStr = [[NSBundle mainBundle] pathForResource:@"jsoc" ofType:@"html"];
     UIWebView *webView = [[UIWebView alloc] initWithFrame:self.view.frame];
-    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:pathStr]]];
+//    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:pathStr]]];
+    
+    // http://m.ixiangha.com:9812/Index/vipDemo   appCommon
+     [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://m.ixiangha.com:9812/Index/vipDemo"]]];
     webView.delegate = self;
     [self.view addSubview:webView];
     NSLog(@"haha");
@@ -40,19 +43,24 @@
 
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [self.view bringSubviewToFront:webView];
     self.jsContext = [webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
     /**
      *  此处将oc中得self传给js对象，jsContext的[@"show"]，这个show任意定义，但是在js中用的时候必须用此一样的名字，来调用我们oc中协议的方法例如上面的hide123，在js中此例子是通过了button的点击方法出发这个协议中得 方法，
      */
     // oc调用js方法
 //    [self.jsContext evaluateScript: @"hide('oc掉jsalert方法')"];
+    
     // oc掉js方法
-    [webView stringByEvaluatingJavaScriptFromString:@"hide()"];
+//    [webView stringByEvaluatingJavaScriptFromString:@"hide()"];
 
     
     
     // js掉oc方法一 通过代理
-    self.jsContext[@"show"] = self;
+//    self.jsContext[@"show"] = self;
+    
+    self.jsContext[@"appCommon"] = self;
+    
     // 方法二通过block
     __weak typeof(self) weakSelf = self;
     self.jsContext[@"hide"] = ^() {
@@ -90,7 +98,16 @@
 }
 
 
-
+- (int)getStatusBarHeight {
+    NSLog(@"getStatusBarHeight");
+//    [self.jsContext evaluateScript:@"foo(1)"];
+    
+    JSValue *callBack = self.jsContext[@"foo"];
+    // 传值给web端
+    [callBack callWithArguments:@[@(1)]];
+    
+    return 20;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
